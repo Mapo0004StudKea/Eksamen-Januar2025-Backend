a8d658b7c31e4680fd1f5cfba584f1c45bf21aab
Martin Poulsen <mapo0004@stud.kea.dk>
2025-01-15 12:13:56 +0100

    add

---
+++ src/main/java/exam/kea/dk/eksamenjanuar2025backend/EksamenJanuar2025BackendApplication.java
@@ -0,0 +1,13 @@
+package exam.kea.dk.eksamenjanuar2025backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamenJanuar2025BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamenJanuar2025BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/exam/kea/dk/eksamenjanuar2025backend/EksamenJanuar2025BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.kea.dk.eksamenjanuar2025backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamenJanuar2025BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
