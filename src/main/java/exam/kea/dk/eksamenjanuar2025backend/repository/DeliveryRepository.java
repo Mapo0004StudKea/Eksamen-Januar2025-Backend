1d280d8c8042543896e468b06e14f8f6360dfa84
Martin Poulsen <mapo0004@stud.kea.dk>
2025-01-16 09:29:38 +0100

    Added repositories
    added repository interfaces

--- src/main/java/exam/kea/dk/eksamenjanuar2025backend/repository/DeliveryRepository.java
+++ src/main/java/exam/kea/dk/eksamenjanuar2025backend/repository/DeliveryRepository.java
@@ -1,9 +1,9 @@
 package exam.kea.dk.eksamenjanuar2025backend.repository;
 
-import exam.kea.dk.eksamenjanuar2025backend.model.Drone;
+import exam.kea.dk.eksamenjanuar2025backend.model.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DroneRepository extends JpaRepository<Drone, Long> {
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
 }
