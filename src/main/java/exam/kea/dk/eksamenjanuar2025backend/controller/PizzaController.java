5f7709a878fe4adbd4cbc0859de0e7326133e104
Martin Poulsen <mapo0004@stud.kea.dk>
2025-01-16 19:51:31 +0100

    updated pizza controller
    updated pizza controller for pagination example

--- src/main/java/exam/kea/dk/eksamenjanuar2025backend/controller/PizzaController.java
+++ src/main/java/exam/kea/dk/eksamenjanuar2025backend/controller/PizzaController.java
@@ -2,13 +2,12 @@ package exam.kea.dk.eksamenjanuar2025backend.controller;
 
 import exam.kea.dk.eksamenjanuar2025backend.model.Pizza;
 import exam.kea.dk.eksamenjanuar2025backend.service.PizzaService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -22,7 +21,7 @@ public class PizzaController {
     }
 
     @GetMapping("/getAll")
-    public ResponseEntity<List<Pizza>> getAll() {
-        return ResponseEntity.ok(pizzaService.getAllPizza());
+    public Page<Pizza> getAll(Pageable pageable) {
+        return pizzaService.getAllPizza(pageable);
     }
 }
--- src/main/java/exam/kea/dk/eksamenjanuar2025backend/service/PizzaService.java
+++ src/main/java/exam/kea/dk/eksamenjanuar2025backend/service/PizzaService.java
@@ -2,20 +2,20 @@ package exam.kea.dk.eksamenjanuar2025backend.service;
 
 import exam.kea.dk.eksamenjanuar2025backend.model.Pizza;
 import exam.kea.dk.eksamenjanuar2025backend.repository.PizzaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class PizzaService {
 
-    private PizzaRepository pizzaRepository;
+    private final PizzaRepository pizzaRepository;
 
     public PizzaService(PizzaRepository pizzaRepository) {
         this.pizzaRepository = pizzaRepository;
     }
 
-    public List<Pizza> getAllPizza() {
-        return pizzaRepository.findAll();
+    public Page<Pizza> getAllPizza(Pageable pageable) {
+        return pizzaRepository.findAll(pageable);
     }
 }
